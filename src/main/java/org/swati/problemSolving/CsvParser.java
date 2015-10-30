2e990d82ef62dacde7aecbbad18101dfac2dc02c
Swati Kumar <swkumar@groupon.com>
2015-10-30 10:52:12 -0700

    Added PatternMatching (naive approach) and Printing the elements of a matrix in a spiral

--- src/main/java/org/swati/problemSolving/CsvParser.java
+++ src/main/java/org/swati/problemSolving/CsvParser.java
@@ -1,4 +1,4 @@
-package org.swati.interview;
+package org.swati.problemSolving;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/org/swati/problemSolving/StringPermutations.java
+++ src/main/java/org/swati/problemSolving/StringPermutations.java
@@ -1,4 +1,4 @@
-package org.swati.interview;
+package org.swati.problemSolving;
 
 import java.util.Arrays;
 import java.util.HashSet;
