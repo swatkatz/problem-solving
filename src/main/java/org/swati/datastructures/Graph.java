9efa3c17d0a3c9a7ca10425e85fb1cbc34cd7bcf
Swati Kumar <swkumar@groupon.com>
2015-12-09 20:35:46 -0800

    Graph representation and Dijkstra's algorithm

---
+++ src/main/java/org/swati/datastructures/Graph.java
@@ -0,0 +1,138 @@
+package org.swati.datastructures;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Directed graph data structure using Adjacency Lists
+ *
+ * @author Swati Kumar
+ * @since 1.0.0
+ */
+public class Graph {
+    private List<Vertex> vertices;
+    private List<Edge> edges;
+
+    public List<Vertex> getVertices() {
+        return vertices;
+    }
+
+    public void setVertices(List<Vertex> vertices) {
+        this.vertices = vertices;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+    public void setEdges(List<Edge> edges) {
+        this.edges = edges;
+    }
+
+    public Set<Vertex> getNeighbors(Vertex vertex) {
+        Set<Vertex> neighbors = new HashSet<Vertex>();
+        if (vertex != null && vertex.getTails() != null) {
+            for (Edge edge : vertex.getTails()) {
+                if (edge != null) {
+                    neighbors.add(edge.getHead());
+                }
+            }
+        }
+        return neighbors;
+    }
+
+    public List<Edge> getDirectPaths(Vertex source, Vertex destination) {
+        //no direct path from source to destination exists
+        if (!getNeighbors(source).contains(destination) || source.getTails() == null) {
+            return new ArrayList<Edge>();
+        }
+        List<Edge> chosenEdges = new ArrayList<Edge>();
+        for (Edge edge : source.getTails()) {
+            if (edge.getHead().equals(destination)) {
+                chosenEdges.add(edge);
+            }
+        }
+        return chosenEdges;
+    }
+}
+
+class Vertex {
+    private char name;
+    private List<Edge> tails;
+    private Integer score = Integer.MAX_VALUE;
+    private Vertex predecessor = null;
+
+    public char getName() {
+        return name;
+    }
+
+    public List<Edge> getTails() {
+        return tails;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public Vertex getPredecessor() {
+        return predecessor;
+    }
+
+    public void setName(char name) {
+        this.name = name;
+    }
+
+    public void setTails(List<Edge> tails) {
+        this.tails = tails;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public void setPredecessor(Vertex predecessor) {
+        this.predecessor = predecessor;
+    }
+
+    @Override
+    public String toString() {
+        return "" + name;
+    }
+}
+
+class Edge {
+    private Vertex head;
+    private Vertex tail;
+    private int weight;
+
+    public Vertex getHead() {
+        return head;
+    }
+
+    public Vertex getTail() {
+        return tail;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setHead(Vertex head) {
+        this.head = head;
+    }
+
+    public void setTail(Vertex tail) {
+        this.tail = tail;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "" + tail + "->" + head;
+    }
+}
